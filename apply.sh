074bc504d2383b5f6e913c9b1777e40a9e21cac0
Hainan Xu <hainan.xv@gmail.com>
2019-06-25 16:44:21 -0400

    adding apply.sh; changing alignmnet params so there are no deletions

--- apply.sh
+++ apply.sh
@@ -3,22 +3,24 @@
 modeldir=output
 text=text
 outdir=bpe_out
-num_bpes=4000
+
+num_bpes=400
 
 . parse_options.sh
 
 mkdir -p $outdir
 
-##cat $text | ./subword-nmt/subword_nmt/apply_bpe.py -c $modeldir/bpe_code.txt > $outdir/original.txt
-##cat $text | sed "s= =\n=g" | grep . | sort | uniq > $outdir/words.txt
+cat $text | sed "s= =\n=g" | grep . > $outdir/words.txt
 
-#phonetisaurus-apply --model g2p_model/model.fst --word_list $outdir/words.txt > $outdir/generated_lexicon.txt
-#../Phonetisaurus/install/bin/phonetisaurus-align --input=$outdir/generated_lexicon.txt --ofile=$outdir/formatted.corpus --seq1_del=false
-#cat $outdir/formatted.corpus | python3 extract.py > $outdir/new_lexicon.txt
-#cat $text | python3 -u process_corpus.py $modeldir/new_lexicon.txt > $outdir/new_text.txt
+phonetisaurus-apply --model g2p_model/model.fst --word_list $outdir/words.txt --lexicon lexicon.txt.formatted > $outdir/generated_lexicon.txt
+../Phonetisaurus/install/bin/phonetisaurus-align --input=$outdir/generated_lexicon.txt --ofile=$outdir/formatted.corpus --seq1_del=false --seq2_del=false
 
-#./subword-nmt/subword_nmt/learn_bpe_phonetic.py -s $num_bpes < $outdir/new_text.txt > $outdir/bpe_code_phonetic.txt
+cat $outdir/formatted.corpus | python3 extract.py > $outdir/new_lexicon.txt
+cat $text | python3 -u process_corpus.py $outdir/new_lexicon.txt > $outdir/new_text.txt
+
+./subword-nmt/subword_nmt/learn_bpe_phonetic.py -s $num_bpes < $outdir/new_text.txt > $outdir/bpe_code_phonetic.txt
+./subword-nmt/subword_nmt/learn_bpe.py -s $num_bpes < $text > $outdir/bpe_code.txt
+
 cat $text | python3 -u process_corpus.py $outdir/new_lexicon.txt | ./subword-nmt/subword_nmt/apply_bpe_phonetic.py -c $outdir/bpe_code_phonetic.txt > $outdir/new_bpe.txt
+cat $text | ./subword-nmt/subword_nmt/apply_bpe.py -c $outdir/bpe_code.txt > $outdir/old_bpe.txt
 
-#cat $text | python3 -u process_corpus.py $modeldir/new_lexicon.txt | ./subword-nmt/subword_nmt/apply_bpe_phonetic.py -c $modeldir/bpe_code_phonetic.txt > $outdir/new_bpe.txt
-#head $text | ./subword-nmt/subword_nmt/apply_bpe.py -c $dir/bpe_code_phonetic.txt
--- train.sh
+++ train.sh
@@ -3,24 +3,11 @@
 dict=lexicon.txt
 text=text
 
-modeldir=output
-
-mkdir -p $modeldir
-
-#text | python3 -u process_corpus.py $modeldir/new_lexicon.txt. parse_options.sh
-#
-#cat $dict | awk '{printf("%s\t", $1); for(i=2;i<=NF;i++) printf("%s ", $i); print""}' | sed "s= $==g" > $modeldir/lexicon.txt
-#../Phonetisaurus/install/bin/phonetisaurus-align --input=$modeldir/lexicon.txt --ofile=$modeldir/formatted.corpus --seq1_del=false
-#
-#cat $modeldir/formatted.corpus | python3 extract.py > $modeldir/new_lexicon.txt
-#
-#cat $text | python3 -u process_corpus.py $modeldir/new_lexicon.txt > $modeldir/text.processed
-#
-#./subword-nmt/subword_nmt/learn_bpe.py -s $num_bpes < $text > $modeldir/bpe_code.txt
-#./subword-nmt/subword_nmt/learn_bpe_phonetic.py -s $num_bpes < $modeldir/text.processed > $modeldir/bpe_code_phonetic.txt
+modeldir=g2p_model
 
 cat $dict | awk '{printf("%s\t",$1);for(i=2;i<=NF;i++) printf("%s ", $i); print("")}' > $dict.formatted
 
+#../Phonetisaurus/install/bin/phonetisaurus-train --lexicon $dict.formatted
 ../Phonetisaurus/install/bin/phonetisaurus-train --lexicon $dict.formatted --seq2_del
 
-mv train g2p_model
+mv train $modeldir
